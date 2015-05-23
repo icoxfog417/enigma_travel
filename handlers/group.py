885faa1db6ec9cb0363a3f4044e3b46dda1559dd
icoxfog417 <icoxfog417@yahoo.co.jp>
2015-05-23 20:01:32 +0900

    create out frame

--- handlers/group.py
+++ handlers/group.py
@@ -1,16 +1,23 @@
+# -*- coding: utf-8 -*-
 import tornado.web
 
 
 class GroupHandler(tornado.web.RequestHandler):
+    """
+    旅行を検討するためのグループを作成する。
+    具体的には、一意のurlを発行する。金額の情報はクエリ引数に込める
+    """
 
     def post(self):
-        # get group name from request
-        group_name = self.get_argument("groupName", "")
+        """
+        グループURLの作成
+        :return:
+        """
+        # create group id and make unique url for travel
+        group_id = "xxx"
         response = {}
 
-        if group_name:
-            # create group
-            url = '//{0}{1}'.format(self.request.host, "/chat/{0}".format("xxx"))
-            response = {"key": "xxx", "url": url}
+        url = '//{0}/travel/{1}'.format(self.request.host, group_id)
+        response = {"group_id": group_id, "url": url}
 
         return self.write(response)

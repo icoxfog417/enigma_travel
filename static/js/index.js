2471e209e29149d9b48411d39f56174c799d2413
icoxfog417 <icoxfog417@yahoo.co.jp>
2015-05-24 12:09:52 +0900

    edit design of top page

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,16 @@
 $(function(){
     createUrl = function(){
-        $.post("/group", {"_xsrf": G.getXsrf()}).done(bindUrl);
+        var budget = $(this).data("value");
+        $.post("/group", {
+                "_xsrf": G.getXsrf(),
+                "budget": budget
+            }
+        ).done(bindUrl);
     }
     bindUrl = function(data){
         $("#share").show();
         $("#url").attr("href", data.url);
+        $("#message").text("今度はみんなが行きたい場所をおしえて？");
     }
-    $("#submit").click(createUrl)
+    $(".submit").click(createUrl)
 })
